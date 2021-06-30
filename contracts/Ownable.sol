c72cc319221c08428065f1ee6995f8129facb0e2
Dimitar Atanasov <dimitar.atanasov@limechain.tech>
2021-06-30 17:43:44 +0300

    Add function returning contract owner

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -3,15 +3,19 @@ pragma solidity ^0.7.0;
 // pragma abicoder v2;
 
 contract Ownable {
-    address public owner;
+    address public _owner;
     
     modifier onlyOwner() {
-        require(owner == msg.sender, "Not invoked by the owner");
+        require(_owner == msg.sender, "Not invoked by the owner");
         _;
     }
         
     constructor() {
-        owner = msg.sender;
+        _owner = msg.sender;
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
     }
  
 }
